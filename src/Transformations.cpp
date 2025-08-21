e82e25514d2569db8a147ba5d638720b0e509b7b
lepton-flux <eric.m.cardozo@gmail.com>
2025-08-20 22:13:42 -0300

    Fix non cuda branch

--- src/Transformations.cpp
+++ src/Transformations.cpp
@@ -20,7 +20,7 @@ namespace cuda {
 using tannic::tensor_t;
 using tannic::stream_t;
 using tannic::device_t;
-inline status gemm(const tensor_t*, const tensor_t*, tensor_t*, stream_t) { throw std::runtime_error("CUDA gemm called without CUDA support"); }
+inline status gemm(const tensor_t*, const tensor_t*, tensor_t*, stream_t, double) { throw std::runtime_error("CUDA gemm called without CUDA support"); }
 inline status outer(tensor_t const*, tensor_t const*, tensor_t*, stream_t) { throw std::runtime_error("CUDA gemm called without CUDA support"); }; 
 inline status repeat(const tensor_t*, tensor_t*, int, int, stream_t) { throw std::runtime_error("CUDA gemm called without CUDA support"); }; 
 inline status concat(const tensor_t*, const tensor_t*, tensor_t*, stream_t, int)  { throw std::runtime_error("CUDA gemm called without CUDA support"); }; 
